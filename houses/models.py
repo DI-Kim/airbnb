cef45b57ed61c16ed7c56a56584a9957ef939412
bigperson <daein7076@gmail.com>
2024-04-18 18:07:29 +0900

    5.6 Recap

--- houses/models.py
+++ houses/models.py
@@ -15,8 +15,8 @@ class House(models.Model):
         default=True, help_text="Does this house allow pets?"
     )
     # on_delete를 통해서 users.User가 삭제되었을 때 house가 어떻게 되는지 설정할 수 있다.
-    # models.CASCADE : user 삭제시 house 삭제
-    # models.SET_NULL : user 삭제시 owner 값을 NULL로 변경
+    # models.CASCADE : user 삭제시(사용자 계정 삭제시) house도 삭제
+    # models.SET_NULL : user 삭제시 house의 owner 값을 NULL로 변경 -> 주인없는 house
     owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
