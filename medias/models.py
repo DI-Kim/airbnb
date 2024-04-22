68820660ab8e1f9a509da35e7b7924200bbfb963
bigperson <daein7076@gmail.com>
2024-04-22 14:05:19 +0900

    6.10 Medias

---
+++ medias/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from common.models import CommonModel
+
+
+# Create your models here.
+class Photo(CommonModel):
+    file = models.ImageField()
+    description = models.CharField(
+        max_length=140,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return "Photo File"
+
+
+class Video(CommonModel):
+    file = models.FileField()
+    # OneToOneField는 ForiegnKey와 비슷하지만, 여러개를 가질 수 있는 ForiegnKey와는 다르게 한개만 가질 수 있다. (unique, 고유한 관계)
+    experience = models.OneToOneField(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Video File"
