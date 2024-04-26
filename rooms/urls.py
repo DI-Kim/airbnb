b83229d918483e73d5d3e31b021382415b3dc2cc
bigperson <daein7076@gmail.com>
2024-04-26 21:31:26 +0900

    9.2 URL Arguments

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.say_hello),
+    path("", views.see_all_rooms),
+    # <>안에 입력받을 파라미터의 타입을 적고 :뒤에 원하는 파라미터 명을 적는다. ex) rooms/1 이면 <int:post_id>, rooms/welcome <str:room_name>
+    path("<int:room_id>", views.see_one_room),
 ]
