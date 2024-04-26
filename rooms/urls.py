29364532257c71d7d292975646f618f4c70eb96d
bigperson <daein7076@gmail.com>
2024-04-26 22:04:31 +0900

    9.6 Django vs React

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,8 +2,4 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path("", views.see_all_rooms),
-    # <>안에 입력받을 파라미터의 타입을 적고 :뒤에 원하는 파라미터 명을 적는다. ex) rooms/1 이면 <int:post_id>, rooms/welcome <str:room_name>
-    path("<int:room_pk>", views.see_one_room),
-]
+urlpatterns = []
