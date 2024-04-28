e8ef72bb98289e41ff97fd2a4222fb951faa17e7
bigperson <daein7076@gmail.com>
2024-04-28 16:04:26 +0900

    10.4 POST Requests

--- categories/views.py
+++ categories/views.py
@@ -5,13 +5,27 @@ from rest_framework.response import Response
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    serializer = CategorySerializer(all_categories, many=True)
-    return Response(
-        {
-            "ok": True,
-            "categories": serializer.data,
-        }
-    )
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(
+            serializer.data,
+        )
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
+
+
+# {
+#     "name": "Category from DRF",
+#     "kind": "rooms"
+# }
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
