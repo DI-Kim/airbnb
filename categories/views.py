4d8d4c79465685aa634069f6df400c6b40b3385b
bigperson <daein7076@gmail.com>
2024-04-28 15:19:21 +0900

    10.2 api_view | before using djangoRestFramework

--- categories/views.py
+++ categories/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Category
 from django.http import JsonResponse
+from django.core import serializers
 
 
 # Create your views here.
 def categories(request):
     all_categories = Category.objects.all()
-    return JsonResponse({"ok": True})
+    return JsonResponse(
+        {
+            "ok": True,
+            "categories": serializers.serialize("json", all_categories),
+        }
+    )
