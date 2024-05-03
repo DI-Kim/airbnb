d22dafc6e0d47278356ede273766b953549d737e
bigperson <daein7076@gmail.com>
2024-05-03 23:25:49 +0900

    16.4 Amenity Detail Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -72,3 +72,40 @@ class TestAmenities(APITestCase):
             "Not 400 status code",
         )
         self.assertIn("name", data)
+
+
+class TestAmenity(APITestCase):
+    NAME = "Test Amenity"
+    DESC = "Test Desc"
+    URL = "/api/v1/rooms/amenities/1"
+    WRONG_URL = "/api/v1/rooms/amenities/12"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_amenity_not_found(self):
+        response = self.client.get(self.WRONG_URL)
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_amenity(self):
+        response = self.client.get(self.URL)
+        data = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["name"], self.NAME)
+        self.assertEqual(data["description"], self.DESC)
+
+    def test_put_amenity(self):
+        # code challenge
+        # serializer가 유효해서 유저가 업데이트 할 수 있는 경우
+        # 유효하지 않을 경우
+        pass
+
+    def test_delete_amenity(self):
+        response = self.client.delete(self.URL)
+
+        self.assertEqual(response.status_code, 204)
