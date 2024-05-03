ee42601130179181cfa4b8255967ffb5473ee9ac
bigperson <daein7076@gmail.com>
2024-05-03 21:59:08 +0900

    15.6 Environment Files | How to use .env file

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,24 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# same path
+# environ.Env.read_env(f"{BASE_DIR}/.env")
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$@&05gp*m^#b5&2afa&ng%cq@2zh&s-wss@3u5l@1(5y$9w44l"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
